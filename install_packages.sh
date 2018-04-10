76e8305245ea644ac399dda2130dc6f8c2eacfa0
TharanyaS <tharanya.selvaraj@maplelabs.com>
2018-04-10 13:29:13 +0530

    keys folder added

--- install_packages.sh
+++ install_packages.sh
@@ -1,3 +1,9 @@
+if [ -f /etc/redhat-release ]; then
+    yum install -y zip unzip
+fi
+if [ -d /etc/apt/ ]; then
+    apt-get install -y zip unzip
+fi
 wget https://releases.hashicorp.com/terraform/0.10.8/terraform_0.10.8_linux_amd64.zip
 unzip terraform_0.10.8_linux_amd64.zip
 mv terraform /usr/local/bin/
